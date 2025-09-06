7cd8eff77b1589840688b81084590f022983c519
RESHETKA <dm0326342@gmail.com>
2025-09-06 20:42:41 +0500

    Add files via upload

--- LIKROOT.py
+++ LIKROOT.py
@@ -5,7 +5,7 @@ for i in range(0, 5):
     print ("")
 
 print ("LIKROOT ВРЕМЕННО ОСТАНОВЛЕН!")
-print ("Разработчик RESHETKA (github Andrey16016)")
+print ("Разработчик> RESHETKA (github: Andrey16016)")
 print ("ТГ> https://t.me/+Q6dkr4rcjJEwY2Qy")
 print ("LIKEE ID> @FEDERAL_OSINT")
 input("Нажмите Enter")
