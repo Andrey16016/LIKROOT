372b47f31a5ddac6e51972c5869cc6c857d51ee1
RESHETKA <dm0326342@gmail.com>
2025-09-06 20:40:39 +0500

    Add files via upload

---
+++ LIKROOT.py
@@ -0,0 +1,12 @@
+#RESHETKA
+#ВРЕМЕННО НЕДОСТУПЕН
+
+for i in range(0, 5):
+    print ("")
+
+print ("LIKROOT ВРЕМЕННО ОСТАНОВЛЕН!")
+print ("Разработчик RESHETKA (github Andrey16016)")
+print ("ТГ> https://t.me/+Q6dkr4rcjJEwY2Qy")
+print ("LIKEE ID> @FEDERAL_OSINT")
+input("Нажмите Enter")
+exit()
